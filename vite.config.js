66b70095ab90f2168b84b55cf57ed02d23850de1
Jesus Lezama <jlezama28@gmail.com>
2026-09-07 10:57:58 +0200

    configurar despliegue para github Page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./", // Usa rutas relativas en los enlaces generados en el HTML
+  base: "/https://github.com/jesusvillarroel/proyecto-Spa-Beauty.git", 
 });
